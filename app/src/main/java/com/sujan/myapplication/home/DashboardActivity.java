318a2c678647e3270b55ca53f3afe9ec93e5b605
sujantuladhar <sujan.tuladhar2013@gmail.com>
2019-12-30 07:57:11 +0545

    Implement tab layout with icon and title

--- app/src/main/java/com/sujan/myapplication/home/DashboardActivity.java
+++ app/src/main/java/com/sujan/myapplication/home/DashboardActivity.java
@@ -1,17 +1,48 @@
 package com.sujan.myapplication.home;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.view.View;
 
+import com.google.android.material.tabs.TabLayout;
 import com.sujan.myapplication.R;
 
 public class DashboardActivity extends AppCompatActivity {
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private Toolbar toolbar;
+    private int[] tabIcons = {
+            R.drawable.ic_tab_favourite,
+            R.drawable.ic_tab_call,
+            R.drawable.ic_tab_contacts
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
-        getSupportFragmentManager().beginTransaction().add(R.id.container,new UserDashboardFragment(),"").addToBackStack("").commit();
+        tabLayout = findViewById(R.id.lytTab);
+        viewPager = findViewById(R.id.viewPager);
+        setupViewPager(viewPager);
+        tabLayout.setupWithViewPager(viewPager);
+        setTabWithIcon();
+//        getSupportFragmentManager().beginTransaction().add(R.id.container,new UserDashboardFragment(),"").addToBackStack("").commit();
+    }
+
+    private void setTabWithIcon(){
+        tabLayout.getTabAt(0).setIcon(tabIcons[0]);
+        tabLayout.getTabAt(1).setIcon(tabIcons[1]);
+        tabLayout.getTabAt(2).setIcon(tabIcons[2]);
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new SalesDashboardFragment(), "");
+        adapter.addFragment(new UserDashboardFragment(), "");
+        adapter.addFragment(new CustomerDashbordFragment(), "");
+        viewPager.setAdapter(adapter);
     }
 }
---
+++ app/src/main/java/com/sujan/myapplication/home/SalesDashboardFragment.java
@@ -0,0 +1,33 @@
+package com.sujan.myapplication.home;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.sujan.myapplication.R;
+
+
+public class SalesDashboardFragment extends Fragment {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sales_dashboard, container, false);
+    }
+
+
+}

b2c24be37794c45d13e8947b7d985da0689f61d5
sujantuladhar <sujan.tuladhar2013@gmail.com>
2020-01-08 08:16:38 +0545

    Implement camera take

--- app/src/main/java/com/sujan/myapplication/LoginActivity.java
+++ app/src/main/java/com/sujan/myapplication/LoginActivity.java
@@ -1,18 +1,24 @@
 package com.sujan.myapplication;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.FileProvider;
 
 import android.Manifest;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.provider.Settings;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -23,10 +29,18 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.sujan.myapplication.category.CategoryActivity;
 import com.sujan.myapplication.home.DashboardActivity;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
     private EditText edtEmailAddress, edtPassword;
@@ -34,6 +48,9 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     private Dialog originalDialog;
     private AlertDialog.Builder alertDialogBuilder;
     private Toolbar toolbar;
+    private ImageView imgPhoto;
+    private Uri picUri;
+    private String currentImagePath;
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
@@ -85,6 +102,36 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         }
     }
 
+    private void checkImagePermission() {
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED || ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED || ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            //   is not granted
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CALL_PHONE)) {
+                AlertDialog alertDialog = new AlertDialog.Builder(LoginActivity.this, R.style.DialogTheme)
+                        .setTitle("Call Permission Needed")
+                        .setMessage("This app needs the Camera and external storage  permission permission , please accept to use camera & gallery functionality")
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                Intent intent = new Intent();
+                                intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                                Uri uri = Uri.fromParts("package", "com.sujan.myapplication", null);
+                                intent.setData(uri);
+                                startActivity(intent);
+                            }
+                        })
+                        .create();
+                alertDialog.setCancelable(false);
+                alertDialog.show();
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 100);
+            }
+        } else {
+            Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + "9864831976"));
+            startActivity(intent);
+        }
+    }
+
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         switch (requestCode) {
@@ -106,10 +153,98 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                         startActivity(intent);
                     }
                 }
+                break;
+            case 100:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[2] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                    if (ActivityCompat.checkSelfPermission(this,
+                            Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,
+                            Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+//                        Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + "9864831976"));
+//                        startActivity(intent);
+                    }
+                } else {
+                    if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA) ||
+                            ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) ||
+                            ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+
+                        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 100);
+                    } else {
+                        Intent intent = new Intent();
+                        intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                        Uri uri = Uri.fromParts("package", "com.sujan.myapplication", null);
+                        intent.setData(uri);
+                        startActivity(intent);
+                    }
+                }
+                break;
         }
 
 
     }
+
+    private void openCamera() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            Intent pictureIntent = new Intent(
+                    MediaStore.ACTION_IMAGE_CAPTURE);
+            if (pictureIntent.resolveActivity(getPackageManager()) != null) {
+                //Create a file to store the image
+                File photoFile = null;
+                try {
+                    photoFile = createImageFile();
+                } catch (IOException ex) {
+                }
+                if (photoFile != null) {
+                    String path = getPackageName() + ".fileprovider";
+                    Log.d("Path", path);
+                    picUri = FileProvider.getUriForFile(this, getPackageName() + ".fileprovider", photoFile);
+                    pictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,
+                            picUri);
+                    startActivityForResult(pictureIntent,
+                            101);
+                }
+
+            }
+        } else {
+            checkImagePermission();
+
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 101) {
+            if (resultCode == RESULT_OK) {
+                File imgFile = new File(currentImagePath);
+                BitmapFactory.Options options = new BitmapFactory.Options();
+                options.inPreferredConfig = Bitmap.Config.ARGB_8888;
+
+                Bitmap bitmap = null;
+                try {
+                    bitmap = BitmapFactory.decodeStream(new FileInputStream(imgFile), null, options);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+                imgPhoto.setImageBitmap(bitmap);
+            }
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                "JPEG_" + timeStamp,
+                ".jpg",
+                storageDir
+        );
+
+        currentImagePath = image.getAbsolutePath();
+        return image;
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -119,6 +254,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         edtEmailAddress = findViewById(R.id.edtEmail);
         edtPassword = findViewById(R.id.edtPassword);
         btnSubmit = findViewById(R.id.btnSubmit);
+        imgPhoto = findViewById(R.id.imgPhoto);
         btnSubmit.setOnClickListener(this);
         edtEmailAddress.addTextChangedListener(new TextWatcher() {
             @Override
@@ -175,6 +311,13 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         TextView txtChooseGallery = dialogView.findViewById(R.id.txtChooseGallery);
         TextView txtCancel = dialogView.findViewById(R.id.txtCancel);
         alertDialogBuilder.setView(dialogView);
+        txtTakePhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                originalDialog.dismiss();
+                openCamera();
+            }
+        });
         originalDialog = alertDialogBuilder.create();
         txtCancel.setOnClickListener(new View.OnClickListener() {
             @Override
